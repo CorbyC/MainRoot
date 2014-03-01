f285fc163d540f2f91d22386dd5329ad470d7b00
CorbyCampbell <Corbyagain@hotmail.com>
2014-02-28 19:43:06 -0700

    First commit
    Created a simple dostuff project

---
+++ DoStuff/DoStuff/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoStuff
+{
+    public partial class Form1 : Form
+    {
+        public static int clicks = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = string.Format("Clicked {0} times", ++clicks);
+        }
+    }
+}
